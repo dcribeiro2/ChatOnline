9e515f6443e5585e66614e5a8e1aeff1164a9f7d
combate <combate@Cavalo_De_Troia>
2017-04-17 16:01:04 -0300

    Chega

--- src/main/java/br/univel/trabalho/Server.java
+++ src/main/java/br/univel/trabalho/Server.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface Server extends Remote {
-//Codigo copiado do ex do prof
+
 	public static final String NOME_SERVICO = "JShare";
 
 	public void registrarCliente(Cliente c) throws RemoteException;
